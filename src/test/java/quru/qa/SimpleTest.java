72e39d9c5364322c75d131e78595b8345ea2f15c
ChesKnow <Roman.Chesanov@outlook.com>
2022-03-07 14:32:10 +0300

    initial commit

---
+++ src/test/java/quru/qa/SimpleTest.java
@@ -0,0 +1,13 @@
+package quru.qa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest {
+
+    @Test
+    void assertTrueTest() {
+        assertTrue(true);
+    }
+}
