5007859a4d23f8f39918b32abe0a15ef266671a1
ChesKnow <Roman.Chesanov@outlook.com>
2022-03-07 15:17:16 +0300

    add assertfalse test

--- src/test/java/quru/qa/SimpleTest.java
+++ src/test/java/quru/qa/SimpleTest.java
@@ -2,6 +2,7 @@ package quru.qa;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SimpleTest {
@@ -9,5 +10,9 @@ public class SimpleTest {
     @Test
     void assertTrueTest() {
         assertTrue(true);
+    }
+
+    void assertFalseTest() {
+        assertFalse(false);
     }
 }
